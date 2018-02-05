c71b37123a8f65ba2c1493755b454a8951f6f207
pdeford <pdeford1@jhu.edu>
2018-02-05 14:03:40 -0500

    Add handling for multiple datasets for K562

--- scripts/preprocess_dnase.sh
+++ scripts/preprocess_dnase.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Usage:
-# ls data/*.bed | cut -d "." -f1 | cut -d "/" -f2 | sort | uniq | while read tf; do bash scripts/preprocess.sh "$tf"; done
-
 tf=$1
 n=$(ls "data/${tf}."*".bed" | wc -l)
 
@@ -16,15 +13,18 @@ else
 	fi
 fi
 
+# Merge all K562 files for TF
+cat "data/${tf}.K562."*".bed" | sortBed -i stdin | mergeBed -i stdin > "data/${tf}.K562.merged.bed"
 
 # Extract the peaks that are not in any of the other files
-bedtools intersect -v -a "data/${tf}.K562."* -b "data/${tf}."[^K]* > "output/unique_${tf}.bed"
+bedtools intersect -v -a "data/${tf}.K562.merged.bed" -b "data/${tf}."[^K]*".bed" > "output/unique_${tf}.bed"
 
 # Extract the peaks that are in every file
-bedtools intersect -c -wa -a "data/${tf}.K562."* -b "data/${tf}."*".bed" | grep $n"$" > "output/ubiq_${tf}.bed"
+bedtools intersect -c -wa -a "data/${tf}.K562.merged.bed" -b "data/${tf}."*".bed" | grep $n"$" > "output/ubiq_${tf}.bed"
 
 # Store the peaks that are in neither all nor none of the other files
-bedtools intersect -v -a "data/${tf}.K562."* -b "output/ubiq_${tf}.bed" "output/unique_${tf}.bed" > "output/med_${tf}.bed"
+bedtools intersect -v -a "data/${tf}.K562.merged.bed" -b "output/ubiq_${tf}.bed" "output/unique_${tf}.bed" > "output/med_${tf}.bed"
 
 # Get some peaks that are not in K562
-bedtools intersect -v -a $(ls "data/${tf}."[^K]* | head -n1) -b "data/${tf}.K562."* > "output/not_K562_${tf}.bed"
+bedtools intersect -v -a $(ls "data/${tf}."[^K]*".bed" | head -n1) -b "data/${tf}.K562.merged.bed" > "output/not_K562_${tf}.bed"
+
