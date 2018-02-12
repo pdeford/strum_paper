53740aa42c3ba96421a150ac8e7e03659c0b4f2c
pdeford <pdeford1@jhu.edu>
2018-02-12 14:11:42 -0500

    Update merging

--- scripts/preprocess_dnase.sh
+++ scripts/preprocess_dnase.sh
@@ -3,13 +3,13 @@
 tf=$1
 n=$(ls "data/${tf}."*".bed" |  cut -f2 -d '.' | sort | uniq | wc -l)
 
-if [ n == '1' ]; then
+if [ $n == '1' ]; then
 	# rm "data/${tf}."[^K]*
 	exit 5
 fi
 
 # Merge all K562 files for TF
-cat "data/${tf}.K562."*".bed" | sortBed -i stdin | mergeBed -i stdin > "data/${tf}.K562.merged.bed"
+cat "data/${tf}.K562."*".bed" | sortBed -i stdin | mergeBed -c 7 -o max -i stdin | sort -k4nr  > "data/${tf}.K562.merged.bed"
 
 # Extract the peaks that are not in any of the other files
 bedtools intersect -v -a "data/${tf}.K562.merged.bed" -b "data/${tf}."[^K]*".bed" > "output/unique_${tf}.bed"
@@ -23,3 +23,4 @@ bedtools intersect -v -a "data/${tf}.K562.merged.bed" -b "output/ubiq_${tf}.bed"
 # Get some peaks that are not in K562
 bedtools intersect -v -a $(ls "data/${tf}."[^K]*".bed" | head -n1) -b "data/${tf}.K562.merged.bed" > "output/not_K562_${tf}.bed"
 
+cat "data/${tf}.K562."*".bed" | sortBed -i stdin | mergeBed -c 7 -o max -i stdin | sort -k4nr  > test.merged.bed
