8c84bf32d5096826e39e7d9a73af2c810a6ca14d
pdeford <pdeford1@jhu.edu>
2019-02-05 14:01:38 -0500

    Automate random seed generation

---
+++ scripts/accession2seed.py
@@ -0,0 +1,10 @@
+import hashlib
+import struct
+import sys
+
+accession = sys.argv[1]
+hashed = hashlib.sha1(accession).digest()
+full_str = '{:0.8e}'.format(struct.unpack('d', hashed[:8])[0])
+strip_str = full_str.replace('.','').replace('-','')
+
+print strip_str[:8]
