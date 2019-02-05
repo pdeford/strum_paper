1c3e34037c7c0bbac221cbb07f5b4a15044f78da
pdeford <pdeford1@jhu.edu>
2019-02-05 14:02:26 -0500

    Create precision recall curves too

--- scripts/logit.py
+++ scripts/logit.py
@@ -12,7 +12,7 @@ import sys
 from multiprocessing import Pool
 from scipy import interp
 from sklearn.linear_model import LogisticRegression as logit
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, auc, precision_recall_curve
 # from sklearn.cross_validation import StratifiedKFold # Version 0.17.1
 from sklearn.model_selection import StratifiedKFold    # Version 0.20.1
 
@@ -69,7 +69,8 @@ def score_all(basename, n_process, random_seed, models, sequences):
 	results = []
 	for i, motif in enumerate([pwm, dwm, ml_strum, em_strum]):
 		fpr, tpr, _ = roc_curve(y, data[:,i])
-		results.append((fpr, tpr, auc(fpr,tpr)))
+		precision, recall, _ = precision_recall_curve(y, data[:,i])
+		results.append((fpr, tpr, auc(fpr,tpr), recall, precision, auc(recall, precision)))
 
 	############## REPEAT FOR OTHER TYPES OF DECOYS #############
 	print >> sys.stderr, "Load other decoys"
@@ -89,7 +90,8 @@ def score_all(basename, n_process, random_seed, models, sequences):
 	working_y[:sum(y)] = 1
 	for i, motif in enumerate([pwm, dwm, ml_strum, em_strum]):
 		fpr, tpr, _ = roc_curve(working_y, working_x[:,i])
-		results2.append((fpr, tpr, auc(fpr,tpr)))
+		precision, recall, _ = precision_recall_curve(working_y, working_x[:,i])
+		results2.append((fpr, tpr, auc(fpr,tpr), recall, precision, auc(recall, precision)))
 
 	##############################################################
 
@@ -105,20 +107,30 @@ def score_all(basename, n_process, random_seed, models, sequences):
 	# cv = StratifiedKFold(y,n_folds=10)  # Version 0.17.1
 	cv = StratifiedKFold(n_splits=10)     # Version 0.20.1
 	tprs = []
+	precisions = []
 	aucs = []
+	prcaucs = []
 	mean_fpr = np.linspace(0, 1, 100)
 	# for train, test in cv:              # Version 0.17.1
 	for train, test in cv.split(data, y): # Version 0.20.1
 		probas_ = clf.fit(data[train], y[train]).predict_proba(data[test])
 		# Compute ROC curve and area the curve
 		fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+		precision, recall, thresholds = precision_recall_curve(y[test], probas_[:, 1])
 		tprs.append(interp(mean_fpr, fpr, tpr))
+		precisions.append(interp(mean_fpr, recall, precision))
 		tprs[-1][0] = 0.0
+		precisions[-1][-1] = 0.0
 		roc_auc = auc(fpr, tpr)
+		precisions[-1][-1] = 0.0
 		aucs.append(roc_auc)
+		prcaucs.append(prcauc)
 	mean_tpr = np.mean(tprs, axis=0)
+	mean_precision = np.mean(precisions, axis=0)
 	mean_tpr[-1] = 1.0
-	results.append((mean_fpr, mean_tpr, np.mean(aucs))) # Combined model
+	mean_precision[0] = 1.0
+	results.append((mean_fpr, mean_tpr, np.mean(aucs), mean_fpr, mean_precision, np.mean(prcaucs))) # Combined model
+
 
 	############## REPEAT FOR OTHER TYPES OF DECOYS #############
 	# Train a combined model using logistic regression
@@ -141,20 +153,31 @@ def score_all(basename, n_process, random_seed, models, sequences):
 		probas_ = clf2.fit(working_x[train], working_y[train]).predict_proba(working_x[test])
 		# Compute ROC curve and area the curve
 		fpr, tpr, thresholds = roc_curve(working_y[test], probas_[:, 1])
+		precision, recall, thresholds = precision_recall_curve(working_y[test], probas_[:, 1])
 		tprs.append(interp(mean_fpr, fpr, tpr))
+		precisions.append(interp(mean_fpr, recall, precision))
 		tprs[-1][0] = 0.0
+		precisions[-1][-1] = 0.0
 		roc_auc = auc(fpr, tpr)
+		precisions[-1][-1] = 0.0
 		aucs.append(roc_auc)
+		prcaucs.append(prcauc)
 	mean_tpr = np.mean(tprs, axis=0)
+	mean_precision = np.mean(precisions, axis=0)
+
 	mean_tpr[-1] = 1.0
-	results2.append((mean_fpr, mean_tpr, np.mean(aucs))) # Combined model
+	mean_precision[0] = 1.0
+	results2.append((mean_fpr, mean_tpr, np.mean(aucs), mean_fpr, mean_precision, np.mean(prcaucs))) # Combined model
+
 	##############################################################
 
 	print >> sys.stderr, "Print Results"
+	# FORMAT:
+	##FACTOR.ACCESSION	pwm-auROC	dwm-auROC	ml_strum-auROC	em_strum-auROC	logit-auROC	pwm-auPRC	dwm-auPRC	ml_strum-auPRC	em_strum-auPRC	logit-auPRC
 	# Output results and plot the results
-	print basename + "\t" + "\t".join(["%0.6f" % x[-1] for x in results])
+	print basename + "\t" + "\t".join(["%0.6f" % x[2] for x in results]) + "\t" + "\t".join(["%0.6f" % x[5] for x in results])
 	############## REPEAT FOR OTHER TYPES OF DECOYS #############
-	print basename + "\t" + "\t".join(["%0.6f" % x[-1] for x in results2])
+	print basename + "\t" + "\t".join(["%0.6f" % x[-1] for x in results2]) + "\t" + "\t".join(["%0.6f" % x[5] for x in results2])
 	#############################################################
 
 	labels = ["PWM",       "DWM",        "ML-StruM", "EM-StruM",  "Combined\nLOGIT"]
@@ -187,8 +210,33 @@ def score_all(basename, n_process, random_seed, models, sequences):
 	plt.savefig("output/{}_ROC2.pdf".format(basename))
 	plt.close()
 
-	return (clf, scaler), (clf2, scaler2), results, results2
+	plt.figure()
+	for i, name in enumerate(labels):
+		row = results[i]
+		plt.plot(row[3], row[4], c=colors[i], label=name)
+	plt.xlim([-0.05, 1.05])
+	plt.ylim([-0.05, 1.05])
+	plt.xlabel('Recall')
+	plt.ylabel('Precision')
+	#plt.title('Receiver operating characteristic example')
+	plt.legend(loc="lower right")
+	plt.savefig("output/{}_PRC.pdf".format(basename))
+	plt.close()
 
+	plt.figure()
+	for i, name in enumerate(labels):
+		row = results[i]
+		plt.plot(row[3], row[4], c=colors[i], label=name)
+	plt.xlim([-0.05, 1.05])
+	plt.ylim([-0.05, 1.05])
+	plt.xlabel('Recall')
+	plt.ylabel('Precision')
+	#plt.title('Receiver operating characteristic example')
+	plt.legend(loc="lower right")
+	plt.savefig("output/{}_PRC2.pdf".format(basename))
+	plt.close()
+
+	return (clf, scaler), (clf2, scaler2), results, results2
 
 def score_pwm(PWM, kmer):
 	"""Score a kmer with a given PWM, and return log2 of the score."""
