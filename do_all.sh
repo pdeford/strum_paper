3c95ce69e87d87a665d44daa0643c0a8bb68ee8e
pdeford <pdeford1@jhu.edu>
2019-02-05 14:01:23 -0500

    simplify running one expt at a time

--- do_all.sh
+++ do_all.sh
@@ -41,71 +41,8 @@ touch output/correlations.txt
 # Loop over each of the ChIP files
 ls data/*K562*bed | while read line;
 do
-	# Iterate seed
-	seed=$(python -c "import hashlib; import struct; print('{:0.31e}'.format(struct.unpack('d', hashlib.sha1('"$accession"').digest()[:8])[0]).replace('.','').replace('e','').replace('-','')[:8])")
-
-	# Extract TF name and accession
-	fname=${line##*/}
-	tf=${fname%%.*}
-	suffix=${fname##*.K562.}
-	accession=${suffix%.*}
-
-	basename=$tf'.'$accession
-
-	# Check if this iteration has already been done. If so,
-	##continue to the next one. (Allows several versions to
-	##run simultaneously.)
-	if [ -e 'data/'$basename'.fa' ]; then
-		continue
-	fi
-
-	echo -n "["$(date +"%F %T")"] "; echo $basename
-
-	# Extract 100bp around the summit
-	sort -k7,7nr $line | awk 'BEGIN{OFS="\t"}{a=($10+$2); print $1, a-50, a+50}' > 'data/'$basename'centered.bed'
-
-	# Prepare sequence data: Extract peaks and flanking sequences, run MEME
-	echo -n "["$(date +"%F %T")"] "; echo "Extract sequence for ChIP peaks"
-	bedtools getfasta -fi data/hg19.fa -bed 'data/'$basename'centered.bed' -fo 'data/'$basename'.fa'
-
-	echo "Get flanking  sequences as negative control"
-	bedtools flank -l 0.0 -r 1.0 -pct -i $line -g data/hg19sizes.genome > $line'.negative'
-	bedtools getfasta -fi data/hg19.fa -bed $line'.negative' -fo 'data/'$basename'.flank.fa'
-
-	echo -n "["$(date +"%F %T")"] "; echo "Run MEME"
-	meme-chip -oc 'output/'$basename'_meme' -dna -nmeme 500 -seed $seed -noecho \
-	  -norand -meme-nmotifs 1 -dreme-m 0 -spamo-skip 'data/'$basename'.fa'
-
-	# Learn each of the motifs: PWM, DWM, ML-StruM, EM-StruM
-	echo -n "["$(date +"%F %T")"] "; echo "Learn motifs and score seqs"
-	python scripts/do_peak_nonpeak.py $basename $n_process $seed >> output/chip_auc.txt
-
-	echo -n "["$(date +"%F %T")"] "; echo "Report coefficients of logit model"
-	./scripts/coeff.py $basename >> output/coefficents.txt
-
-	echo -n "["$(date +"%F %T")"] "; echo "Compare positioning of most significant matches"
-	./scripts/compare_positions.py $basename $n_process >> output/position_comp.txt
-
+	bash scripts/do_one.sh $n_process $line
 done
-
-##############################################################
-# Find the correlation between scores assigned by each model #
-##############################################################
-
-# Loop over each of the ChIP files
-ls data/*K562*bed | while read line;
-do
-	# Extract TF name and accession
-	fname=${line##*/}
-	tf=${fname%%.*}
-	suffix=${fname##*.K562.}
-	accession=${suffix%.*}
-
-	basename=$tf'.'$accession
-
-	echo "Get correlation between motif scores"
-	./scripts/correlation.py $basename >> output/correlations.txt
-done;
 
 ####################################
 # Generate the figures from output #
---
+++ scripts/do_one.sh
@@ -0,0 +1,60 @@
+
+# Check command line arguments
+if [[ $# -ne 2 ]]; then
+    echo "ERROR: Illegal number of parameters:" $#
+    echo "Usage:"
+    echo "    ${0} n_process line"
+    exit 5
+fi
+
+# Read command line arguments
+n_process=$1
+line=$2
+
+# Extract TF name and accession
+fname=${line##*/}
+tf=${fname%%.*}
+suffix=${fname##*.K562.}
+accession=${suffix%.*}
+
+basename=$tf'.'$accession
+
+# Set random seed for reproducibility
+seed=$(python scripts/accession2seed.py $accession)
+
+# Check if this iteration has already been done. If so,
+##continue to the next one. (Allows several versions to
+##run simultaneously.)
+if [ -e 'data/'$basename'.fa' ]; then
+	continue
+fi
+
+echo -n "["$(date +"%F %T")"] "; echo $basename
+
+# Extract 100bp around the summit
+sort -k7,7nr $line | awk 'BEGIN{OFS="\t"}{a=($10+$2); print $1, a-50, a+50}' > 'data/'$basename'centered.bed'
+
+# Prepare sequence data: Extract peaks and flanking sequences, run MEME
+echo -n "["$(date +"%F %T")"] "; echo "Extract sequence for ChIP peaks"
+bedtools getfasta -fi data/hg19.fa -bed 'data/'$basename'centered.bed' -fo 'data/'$basename'.fa'
+
+echo "Get flanking  sequences as negative control"
+bedtools flank -l 0.0 -r 1.0 -pct -i $line -g data/hg19sizes.genome > $line'.negative'
+bedtools getfasta -fi data/hg19.fa -bed $line'.negative' -fo 'data/'$basename'.flank.fa'
+
+echo -n "["$(date +"%F %T")"] "; echo "Run MEME"
+meme-chip -oc 'output/'$basename'_meme' -dna -nmeme 500 -seed $seed -noecho \
+  -norand -meme-nmotifs 1 -dreme-m 0 -spamo-skip 'data/'$basename'.fa'
+
+# Learn each of the motifs: PWM, DWM, ML-StruM, EM-StruM
+echo -n "["$(date +"%F %T")"] "; echo "Learn motifs and score seqs"
+python scripts/do_peak_nonpeak.py $basename $n_process $seed >> output/chip_auc.txt
+
+echo -n "["$(date +"%F %T")"] "; echo "Report coefficients of logit model"
+./scripts/coeff.py $basename >> output/coefficents.txt
+
+echo -n "["$(date +"%F %T")"] "; echo "Compare positioning of most significant matches"
+./scripts/compare_positions.py $basename $n_process >> output/position_comp.txt
+
+echo "Get correlation between motif scores"
+./scripts/correlation.py $basename >> output/correlations.txt
